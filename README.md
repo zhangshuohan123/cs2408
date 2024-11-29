07d40b0931989be1fac49a0f21647438c9a124a0
ztsubaki <2949125232@qq.com>
2024-11-29 17:30:17 +0800

    Add random recommend, fix some responsive problem.

--- README.md
+++ README.md
@@ -14,11 +14,13 @@
       "members": [
         {
           "name": "",
-          "url": ""
+          "url": "",
+          "intro": ""
         },
         {
           "name": "",
-          "url": ""
+          "url": "",
+          "intro": ""
         }
       ]
     },{
@@ -28,17 +30,21 @@
       "members": [
         {
           "name": "",
-          "url": ""
+          "url": "",
+          "intro": ""
         },
         {
           "name": "",
-          "url": ""
+          "url": "",
+          "intro": ""
         }
       ]
     }
   ]
 ````
-找到`dormitories`数组中`name`对应的寝室名称，在`sign`中写入寝室介绍，`url`中粘贴寝室页面的完整URL,`members`数组对应寝室中的成员，在其中的`url`条目写入寝室成员网页的URL即可。
+找到`dormitories`数组中`name`对应的寝室名称，在`sign`中写入寝室介绍，`url`中粘贴寝室页面的完整URL,`members`数组对应寝室中的成员，在其中的`url`条目写入寝室成员网页的URL，并在`intro`条目中写入网页标题或简介即可。
+
+优秀作业在`exhibitions`数组中按相同方式配置，随机推荐会根据`rand_num`中配置的数目每小时更新
 ## Libraries
 - jQuery 3.6.0
 - Vue 3.2.31
--- js/index.js
+++ js/index.js
@@ -4,6 +4,21 @@ $.ajax({
     url: "./api/index.json", dataType: 'json',
     success: function (result) {
         re = $.parseJSON(JSON.stringify(result));
+        var i = 0;
+        for (var k = 0; k < re.rand_num; k++) {
+            while (true) {
+                let dormitory = rnd(i++) % re.dormitories.length;
+                dormitory = re.dormitories[dormitory];
+                let member = rnd(i++) % dormitory.members.length;
+                member = dormitory.members[member];
+                if (!member.visited) {
+                    member.dormintory = dormitory.name;
+                    re.randoms[k] = member;
+                    break;
+                }
+            }
+        }
+
     }, error: function (error) {
         console.log(error);
         location.replace("./api/index.json");
@@ -13,8 +28,30 @@ $.ajax({
 
 window.onload = function () {
     Vue.createApp({
-        data: function (){
+        data: function () {
             return re;
         }
     }).mount("#vm");
+    let py = $(document).scrollTop();
+    $(window).scroll(function () {
+        if(($(document).scrollTop() > $("#title").height()))
+        {
+            if(($(document).scrollTop()-py)<-20){
+                $("#nav_bar").removeClass("nav_invisible").addClass("nav_visible");}
+            if(($(document).scrollTop()-py)>20){
+                $("#nav_bar").removeClass("nav_visible").addClass("nav_invisible");
+            }
+        }else{
+            $("#nav_bar").removeClass("nav_visible").addClass("nav_invisible");
+        }
+        py = $(document).scrollTop();
+    });
+}
+
+
+function rnd(seed) {
+    const d = new Date();
+    seed = d.getFullYear() * 8928 + d.getMonth() * 744 + d.getDate()*24 + seed;
+    seed = (seed * 9301 + 49297) % 233283;
+    return seed;
 }
